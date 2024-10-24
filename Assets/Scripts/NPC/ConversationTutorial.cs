b11d2695a78d966ee0bdec9fbd10b1cbcbe4df57
Germaine Whilby II <grw4@pct.edu>
2024-10-24 16:53:18 -0400

    Cooking
    Added barriers
    Have to give arguments to the tutorial boxes to allow for next barrier

--- Assets/Scripts/NPC/ConversationTutorial.cs
+++ Assets/Scripts/NPC/ConversationTutorial.cs
@@ -15,18 +15,11 @@ public class ConversationTutorial : MonoBehaviour
         if (other.CompareTag("Player"))
         {
                 ConversationManager.Instance.StartConversation(myConversation);
-                ConversationManager.Instance.SetInt("Gold", PlayerController.goldCount);
                 ConversationManager.Instance.SetBool("Bought1", PlayerController.Bought1);
                 ConversationManager.Instance.SetBool("Bought2", PlayerController.Bought2);
                 ConversationManager.Instance.SetBool("Bought3", PlayerController.Bought3);
                 ConversationManager.Instance.SetBool("Bought4", PlayerController.Bought4);
                 ConversationManager.Instance.SetBool("Bought5", PlayerController.Bought5);
-                ConversationManager.Instance.SetBool("Bought6", PlayerController.Bought6);
-                ConversationManager.Instance.SetBool("Bought7", PlayerController.Bought7);
-                ConversationManager.Instance.SetBool("Bought8", PlayerController.Bought8);
-                ConversationManager.Instance.SetBool("LaserBought", PlayerController.LaserBought);
-                ConversationManager.Instance.SetBool("IceBought", PlayerController.IceBought);
-                ConversationManager.Instance.SetBool("ExplosiveBought", PlayerController.ExplosiveBought);
 
         }
 
