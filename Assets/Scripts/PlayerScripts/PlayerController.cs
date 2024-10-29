4f3b6449c9dcb64968704a2f3eb7efbb543c35b0
Germaine Whilby II <grw4@pct.edu>
2024-10-29 16:20:29 -0400

    Set Start Gold to 0
    Gotta start at ground 0

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -21,7 +21,7 @@ public class PlayerController : MonoBehaviour
     private Vector3 moveDirection = Vector3.zero;
     private float rotationX = 0;
 
-    public static int goldCount = 5000;
+    public static int goldCount = 0; //Got to start at ground zero
 
     //Stats that can be changed buy NPCs
     public int maxHealth = 100;
