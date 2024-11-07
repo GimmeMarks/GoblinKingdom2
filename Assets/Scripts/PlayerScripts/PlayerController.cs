4a87b1897a062d9faa2d7dd73192a01c52320f70
Germaine Whilby II <grw4@pct.edu>
2024-11-07 10:49:01 -0500

    Known Bugs
    Cant switch from 2 and 4
    Mana not decreasing when firing

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -15,7 +15,7 @@ public class PlayerController : MonoBehaviour
     public float gravity;
     public float speed = 5.0f;
     public float jumpForce = 3f;
-    public static int goldCount = 100; //Got to start at ground zero
+    public static int goldCount = 500; //Got to start at ground zero
     [Tab("Serializables")]
 
     public TMP_Text waveNumberUI;
