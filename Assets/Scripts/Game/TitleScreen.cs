613c6fbc677de3e0ca2a5de69ec5c2f98720c823
Germaine Whilby II <grw4@pct.edu>
2024-11-14 09:21:05 -0500

    Added Tutorial on Title
    Can now access the tutorial on title screen

--- Assets/Scripts/Game/TitleScreen.cs
+++ Assets/Scripts/Game/TitleScreen.cs
@@ -9,17 +9,23 @@ public class TitleScreen : MonoBehaviour
 {
 
     [SerializeField] Button Play;
+    [SerializeField] Button Tutorial;
     [SerializeField] Button Quit;
 
     private void Awake()
     {
         Play.onClick.AddListener(playClick);
+        Tutorial.onClick.AddListener(tutorialClick);
         Quit.onClick.AddListener(quitClick);
     }
 
     void playClick()
     {
         SceneManager.LoadScene("MainGame");
+    }
+    void tutorialClick()
+    {
+        SceneManager.LoadScene("Tutorial");
     }
     void quitClick()
     {
